fac6fbaa76f1291080d6e4cbb39a0f40adacbcc7
Octavian Popa <octavian.popa@windeln.ro>
2016-10-04 11:45:41 +0300

    Fixed app.component.html location ref

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -2,7 +2,7 @@ import {Component} from "@angular/core";
 
 @Component({
     selector: 'books-app',
-    templateUrl: 'app/app.component.html'
+    templateUrl: 'app/components/app.component.html'
 })
 export class AppComponent {
 
