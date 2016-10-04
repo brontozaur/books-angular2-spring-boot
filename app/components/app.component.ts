62fb4b55f711b1225100d17b3ae99383136789bd
Octavian Popa <octavian.popa@windeln.ro>
2016-10-04 11:44:10 +0300

    Changed structure a bit

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -3,7 +3,6 @@ import {Component} from "@angular/core";
 @Component({
     selector: 'books-app',
     templateUrl: 'app/app.component.html'
-    // template: "Hello {{title}}"
 })
 export class AppComponent {
 
